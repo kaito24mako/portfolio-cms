5d8cea928d5ccbc01cafe3d6f551cc72cc327da7
Kaito Watanabe <kaitowatanabemcc@gmail.com>
2026-04-28 15:57:40 +1000

    put bg image in layout to be app-wide

--- app/(app)/layout.js
+++ app/(app)/layout.js
@@ -1,10 +1,20 @@
 import Sidebar from "../_components/_layout/Sidebar";
+import bgWaves from "@/public/bg/waves.svg";
+import Image from "next/image";
 
 export default function AppLayout({ children }) {
   return (
     <Sidebar>
-      <div className="flex-1 p-4">
-        <main>{children}</main>
+      <div className="relative flex-1 p-4">
+        <Image
+          src={bgWaves}
+          fill
+          alt=""
+          className="object-cover object-top pointer-events-none"
+        />
+        <main className="relative z-5 px-4 lg:px-10 xl:px-16 2xl:px-36">
+          {children}
+        </main>
       </div>
     </Sidebar>
   );
--- app/(app)/page.js
+++ app/(app)/page.js
@@ -1,10 +1,7 @@
-import bgWaves from "@/public/bg/waves.svg";
 import CardGrid from "../_components/_ui/CardGrid";
 import CardSmall from "../_components/_ui/CardSmall";
 import Statistic from "../_components/_ui/Statistic";
 import Title from "../_components/_ui/Title";
-
-import Image from "next/image";
 
 export const metadata = {
   title: "Dashboard",
@@ -12,86 +9,77 @@ export const metadata = {
 
 export default function Dashboard() {
   return (
-    <div>
-      <Image
-        src={bgWaves}
-        fill
-        alt=""
-        className="object-cover object-top z-0 pointer-events-none"
+    <div className="flex flex-col gap-8">
+      {/* title */}
+      <Title
+        heading="Hello Kaito,"
+        subHeading="Let's create a beautiful portfolio."
       />
 
-      <div className="relative z-5 px-4 lg:px-10 xl:px-16 2xl:px-36 flex flex-col gap-8">
-        {/* title */}
-        <Title
-          heading="Hello Kaito,"
-          subHeading="Let's create a beautiful portfolio."
-        />
+      {/* projects cards */}
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between items-center">
+          <h2 className="text-lg font-medium">Your Portfolio</h2>
+          <span className="text-sm">Last updated: 2 hours ago</span>
+        </div>
+        <CardGrid>
+          <CardSmall
+            imgAlt="Book Library project"
+            title="Book Library"
+            description="Description of the project"
+            btnText="Edit Project"
+          />
+          <CardSmall
+            imgAlt="Book Library project"
+            title="Book Library"
+            description="Description of the project"
+            btnText="Edit Project"
+          />
+          <CardSmall
+            imgAlt="Book Library project"
+            title="Book Library"
+            description="Description of the project"
+            btnText="Edit Project"
+          />
+          <CardSmall
+            imgAlt="Book Library project"
+            title="Book Library"
+            description="Description of the project"
+            btnText="Edit Project"
+          />
+          <CardSmall
+            imgAlt="Book Library project"
+            title="Book Library"
+            description="Description of the project"
+            btnText="Edit Project"
+          />
+          <CardSmall
+            imgAlt="Book Library project"
+            title="Book Library"
+            description="Description of the project"
+            btnText="Edit Project"
+          />
+        </CardGrid>
+      </div>
 
-        {/* projects cards */}
-        <div className="flex flex-col gap-2">
-          <div className="flex justify-between items-center">
-            <h2 className="text-lg font-medium">Your Portfolio</h2>
-            <span className="text-sm">Last updated: 2 hours ago</span>
+      <div className="grid grid-cols-5">
+        {/* statistics */}
+        <div className="flex flex-col gap-2 col-span-2">
+          <h2 className="text-lg font-medium">Statistics</h2>
+          <div className="flex flex-col gap-3">
+            <Statistic title="Total Projects" count={6} />
+            <Statistic title="Published Projects" count={3} />
+            <Statistic title="Unique Tags" count={5} />
           </div>
-          <CardGrid>
-            <CardSmall
-              imgAlt="Book Library project"
-              title="Book Library"
-              description="Description of the project"
-              btnText="Edit Project"
-            />
-            <CardSmall
-              imgAlt="Book Library project"
-              title="Book Library"
-              description="Description of the project"
-              btnText="Edit Project"
-            />
-            <CardSmall
-              imgAlt="Book Library project"
-              title="Book Library"
-              description="Description of the project"
-              btnText="Edit Project"
-            />
-            <CardSmall
-              imgAlt="Book Library project"
-              title="Book Library"
-              description="Description of the project"
-              btnText="Edit Project"
-            />
-            <CardSmall
-              imgAlt="Book Library project"
-              title="Book Library"
-              description="Description of the project"
-              btnText="Edit Project"
-            />
-            <CardSmall
-              imgAlt="Book Library project"
-              title="Book Library"
-              description="Description of the project"
-              btnText="Edit Project"
-            />
-          </CardGrid>
         </div>
 
-        <div className="grid grid-cols-5">
-          {/* statistics */}
-          <div className="flex flex-col gap-2 col-span-2">
-            <h2 className="text-lg font-medium">Statistics</h2>
-            <div className="flex flex-col gap-3">
-              <Statistic title="Total Projects" count={6} />
-              <Statistic title="Published Projects" count={3} />
-              <Statistic title="Unique Tags" count={5} />
-            </div>
+        {/* recent activity */}
+        <div className="flex flex-col gap-2 col-span-3">
+          <div className="flex justify-between items-center">
+            <h2 className="text-lg font-medium">Recent Activity</h2>
+            <button className="cursor-pointer text-sm">View All</button>
           </div>
-
-          {/* recent activity */}
-          <div className="flex flex-col gap-2 col-span-3">
-            <div className="flex justify-between items-center">
-              <h2 className="text-lg font-medium">Recent Activity</h2>
-              <button className="cursor-pointer text-sm">View All</button>
-            </div>
-            <div className="border border-base-300 rounded">activity</div>
-          </div>
+          <div className="border border-base-300 rounded">activity</div>
         </div>
       </div>
     </div>
