4a35ee126668e96f8eb6390e1747132de96d3fb0
Kaito Watanabe <kaitowatanabemcc@gmail.com>
2026-05-08 00:29:00 +1000

    bug with locating pg

--- utils/connection.js
+++ utils/connection.js
@@ -1,9 +1,11 @@
 import { Sequelize } from "sequelize";
+import pg from "pg";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   // change to postgres for supabase
   // tells sequelize to connect to Postgres
   dialect: "postgres",
+  dialectModule: pg,
   logging: false,
   dialectOptions: {
     ssl:
