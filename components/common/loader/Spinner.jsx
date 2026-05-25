d6a6fda88567569d4c07fa4934e376791f20fc31
Kaito Watanabe <kaitowatanabemcc@gmail.com>
2026-05-25 23:31:32 +1000

    reduce opacity of spinner

--- components/common/loader/Spinner.jsx
+++ components/common/loader/Spinner.jsx
@@ -1,7 +1,14 @@
 import { ClipLoader } from "react-spinners";
 
 function Spinner({ size = 40 }) {
-  return <ClipLoader color="currentColor" size={size} speedMultiplier={0.6} />;
+  return (
+    <ClipLoader
+      color="currentColor"
+      size={size}
+      speedMultiplier={0.6}
+      cssOverride={{ opacity: 0.5 }}
+    />
+  );
 }
 
 export default Spinner;
