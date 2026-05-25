0f4255297f5f2228539ddf172aa5dd5629839e62
Kaito Watanabe <kaitowatanabemcc@gmail.com>
2026-05-26 00:32:34 +1000

    move text up on loading.jsx

--- app/(app)/projects/[id]/edit/loading.jsx
+++ app/(app)/projects/[id]/edit/loading.jsx
@@ -3,7 +3,7 @@ import Spinner from "@/components/common/loader/Spinner";
 function loading() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="-translate-y-24 flex flex-col gap-3 items-center">
+      <div className="-translate-y-45 flex flex-col gap-3 items-center">
         <span className="text-xl skeleton skeleton-text">
           Loading project data...
         </span>
