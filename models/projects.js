b4efa9a41aea831a0a0ea5c65d936e2bdf2d73e1
Kaito Watanabe <kaitowatanabemcc@gmail.com>
2026-06-02 10:35:03 +1000

    further change date formatting

--- models/projects.js
+++ models/projects.js
@@ -37,26 +37,17 @@ const Project = sequelize.define("Project", {
   updatedAt: {
     type: DataTypes.DATE,
     get() {
-      const rawValue = this.getDataValue("updatedAt");
-      if (!rawValue) return null;
+      const date = this.getDataValue("updatedAt");
+      if (!date) return null;
 
-      const date = new Date(rawValue);
-      const currentYear = new Date().getFullYear();
+      const day = date.getDate();
 
-      const dateOptions = {
-        month: "short",
-        day: "numeric",
-      };
+      const month = date.toLocaleString("en-AU", {
+        month: "long",
+      });
 
-      // only show year if it's not the current year
-      if (date.getFullYear() !== currentYear) {
-        dateOptions.year = "numeric";
-      }
-
-      const formattedDate = date.toLocaleDateString(undefined, dateOptions);
-
-      const formattedTime = date
-        .toLocaleTimeString(undefined, {
+      const time = date
+        .toLocaleTimeString("en-AU", {
           hour: "numeric",
           minute: "2-digit",
           hour12: true,
@@ -64,7 +55,7 @@ const Project = sequelize.define("Project", {
         .replace(" ", "")
         .toLowerCase();
 
-      return `${formattedDate}, ${formattedTime}`;
+      return `${month} ${day}, ${time}`;
     },
   },
 });
